cb370375db4824a87a7c161c31a76d56f727913b
GunabalanLingam <gunabalanbh1942@gmail.com>
2025-06-09 22:18:30 +0100

    Fixed the candidate page not loading problem

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,8 +1,3 @@
 // src/baseURL.js
-const isLocal = window.location.hostname === "localhost";
-
-const BASE_URL = isLocal
-    ? "http://localhost:5000"                         // Local backend
-    : "https://resume-webhook.onrender.com";         // Deployed backend
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default BASE_URL;
